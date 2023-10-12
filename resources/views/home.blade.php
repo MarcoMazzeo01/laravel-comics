d6feb64113dfeaa698ac5c28b95a684a3b42cf1c
plastikSun <marcom2k01@gmail.com>
2023-10-12 18:40:18 +0200

    base structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>Laravel Comics</title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -15,7 +15,9 @@
 </head>
 
 <body>
-  <h1>ciao!</h1>
+  <header>Questo è un header</header>
+  <main>Questo è un main</main>
+  <footer>Questo è un footer</footer>
 </body>
 
 </html>
