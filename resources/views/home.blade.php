eb1eea172b09c46bdd5f5f70b0d40e3a6a956ff9
plastikSun <marcom2k01@gmail.com>
2023-10-12 18:50:36 +0200

    add: @yield and @section testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('page-title')
+    Home 
+@endsection
 
-  <title>Laravel Comics</title>
+@section('main-content')
+    TEST
 
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-  <!-- Styles -->
-  @vite('resources/js/app.js')
-</head>
-
-<body>
-  <header>Questo è un header</header>
-  <main>Questo è un main</main>
-  <footer>Questo è un footer</footer>
-</body>
-
-</html>
+@endsection 
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel Comics - @yield('page-title')</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+  <!-- Styles -->
+  @vite('resources/js/app.js')
+</head>
+
+<body>
+  <header>Questo è un header</header>
+  <main>@yield('main-content')</main>
+  <footer>Questo è un footer</footer>
+</body>
+
+</html>
